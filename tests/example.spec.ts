de4e0387a5f0bd3fc803602352bc6b74bf542339
oleboifsh <olesia.boiarchuk@elevmail.stockholm.se>
2024-11-14 23:11:23 +0100

    commit with password

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,9 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  console.log("The value is: " + process.env.PASSWORD);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwrightuuu/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
