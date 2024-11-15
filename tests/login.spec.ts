01f4b98bf76d5241eabdb5f70155784001537526
oleboifsh <olesia.boiarchuk@elevmail.stockholm.se>
2024-11-15 23:09:44 +0100

    read password from GIT

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,9 +2,13 @@
 import test from "@playwright/test";
 import { LoginPage } from "../pages/loginpage";
 
+
+let password: string
 test('Login with Markus', async ({page}) => {
     const loginPage = new LoginPage(page)
     await page.goto("https://hoff.is/login")
-    await loginPage.login("Markus", "sup3rs3cr3t")
-
+    if (process.env.PASSWORD !== undefined) {
+        password = process.env.PASSWORD
+    }
+    await loginPage.login("Markus", password)
 });
